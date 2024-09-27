56810975ac0ffd8d407af63eec7aa8a1ad106331
gustavonrm <gustavo.magalhaes@jumpseller.com>
2024-09-27 19:09:43 +0100

    all tests passing adding the missing information on preview screen now i will just add CSS

--- src/components/Hotel/HotelPreview/index.tsx
+++ src/components/Hotel/HotelPreview/index.tsx
@@ -4,6 +4,8 @@ import { selectHotel } from '../../../redux/slices/hotels.slice';
 import withError from '../../HOC/withError';
 import { useSelector } from 'react-redux';
 import style from './style';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { YELLOW } from '../../../constants/colors';
 
 type HotelPreviewProps = {
   id: number;
@@ -13,7 +15,7 @@ const HotelPreview = ({ id }: HotelPreviewProps) => {
   const hotel = useSelector((state) => selectHotel(id)(state));
 
   return (
-    <View>
+    <ScrollView testID="hotelPreviewTestId">
       <ScrollView horizontal>
         {hotel?.gallery.map((image, index) => (
           <Image
@@ -28,7 +30,29 @@ const HotelPreview = ({ id }: HotelPreviewProps) => {
         ))}
       </ScrollView>
       <Text>{hotel?.name}</Text>
-    </View>
+      <View style={style.stars}>
+        {Array.from({ length: hotel.stars }).map((_, index) => (
+          <FontAwesomeIcon testID={'starTestId'} key={index} icon="star" color={YELLOW} />
+        ))}
+      </View>
+      <Text style={style.text3}> | {hotel.userRating} user rating</Text>
+      <Text>{'Address Information:'}</Text>
+      <Text>{`${hotel.location.city}, ${hotel.location.address}`}</Text>
+      <Text>{`Latitude: ${hotel.location.latitude}`}</Text>
+      <Text>{`Longitude: ${hotel.location.latitude}`}</Text>
+
+      <Text>{`Check in: ${hotel.checkIn.from}-${hotel.checkIn.to}`}</Text>
+      <Text>{`Check out: ${hotel.checkOut.from}-${hotel.checkOut.to}`}</Text>
+
+      <Text>{'Contact Information:'}</Text>
+      <Text>{`Phone Number: ${hotel.contact.phoneNumber}`}</Text>
+      <Text>{`Email: ${hotel.contact.email}`}</Text>
+
+      <Text>{`Book Hotel`}</Text>
+      <Text>{`€${hotel.price} /person`}</Text>
+
+      <Text>{`€${hotel.price} total`}</Text>
+    </ScrollView>
   );
 };
 
